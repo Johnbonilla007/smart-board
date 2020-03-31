7ad90e9f2af01e7d81265f865ff7c28193076c5b
Joann <joannmurillo81@gmail.com>
2020-03-31 03:38:29 -0600

    Navegacion, Hamburguer-menu y menu superior añadido

---
+++ src/Resources/data.js
@@ -0,0 +1,44 @@
+export const menuItems = [
+  {
+    name: "Tareas",
+    backgroundColor: "#00bfff",
+    iconName: "assignment",
+    key: "task"
+  },
+  {
+    name: "Horario de clases",
+    backgroundColor: "#fc4903",
+    iconName: "schedule",
+    key: "classSchedule"
+  },
+  {
+    name: "Eeventos",
+    backgroundColor: "#5972FF",
+    iconName: "event",
+    key: "events"
+  },
+  {
+    name: "Calificaciones",
+    backgroundColor: "#50C98F",
+    iconName: "graphic-eq",
+    key: "schoolGrades"
+  },
+  {
+    name: "Mensajes",
+    backgroundColor: "#FF913D",
+    iconName: "message",
+    key: "messages"
+  },
+  {
+    name: "Asistencia",
+    backgroundColor: "#F959BC",
+    iconName: "perm-identity",
+    key: "asistence"
+  },
+  {
+    name: "Calendario de Examenes",
+    backgroundColor: "#CC222B",
+    iconName: "date-range",
+    key: "examSchedule"
+  }
+];
